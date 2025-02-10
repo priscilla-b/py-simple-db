ffe7917cd93ae461963e59e274d83c08f0a294e5
Priscilla Baah <pyfbaah@gmail.com>
2025-02-10 09:57:23 +0000

    add server module

---
+++ simple-db/server/simpledb.py
@@ -0,0 +1,25 @@
+import os
+import logging
+from pathlib import Path
+from file.file_manager import FileManager
+
+
+
+class SimpleDB:
+    BLOCK_SIZE = 400
+    BUFFER_SIZE = 8
+    LOG_FILE = 'simpledb.log'
+    
+    def __init__(self, dirname, block_size=BLOCK_SIZE, buffer_size=BUFFER_SIZE):
+        """
+        Initializes the SimpleDB engine.
+        
+        :param dirname: Name of the database directory
+        :param block_size: Size of database blocks
+        :param buffer_size: Number of buffers
+        """
+        
+        self.db_directory = Path(dirname)
+        self.db_directory.mkdir(parents=True, exist_ok=True)
+        
+        self.file_mgr = FileManager(self.db_directory, block_size)
