8dbe6df56012e7586351386456011c65b524c83e
Priscilla Baah <pyfbaah@gmail.com>
2025-02-20 23:26:08 +0000

    recovery manager in progress

--- simple-db/transaction/recovery/recovery_manager.py
+++ simple-db/transaction/recovery/recovery_manager.py
@@ -1,21 +1,38 @@
+from start_record import StartRecord
+from buffer.buffer_manager import BufferManager
+from log.log_manager import LogManager
+from commit_record import CommitRecord
+from rollback_record import RollbackRecord
+from transaction.transaction import Transaction
 
 class RecoveryManager:
-    def __init__(self, transaction, tx_number, log_manager, buffer_manager):
+    def __init__(self, transaction:Transaction, tx_number, log_manager:LogManager, buffer_manager:BufferManager):
+        """
+        Create a recovery manager for the specified transaction.
+        :param tx_number: the id of the specified transaction
+        """
         self.transaction = transaction
         self.tx_number = tx_number
         self.log_manager = log_manager
         self.buffer_manager = buffer_manager
 
-    def analyze(self):
-        pass
-
-    def redo(self):
-        pass
-
-    def undo(self):
-        pass
-
-    def recover(self):
-        self.analyze()
-        self.redo()
-        self.undo()
+        StartRecord.write_to_log(log_manager, tx_number)
+        
+    def commit(self):
+        """
+        Writes a commit record to the log and flushes the log to disk.
+        """
+        self.buffer_manager.flush_all(self.tx_number)
+        lsn = CommitRecord.write_to_log(self.log_manager, self.tx_number)
+        self.log_manager.flush(lsn)
+        
+    def rollback(self):
+        """
+        Write a rollback record to the log and flush it to disk.
+        """
+        self.do_rollback()
+        self.buffer_manager.flush_all(self.tx_number)
+        lsn = RollbackRecord.write_to_log(self.log_manager, self.tx_number)
+        self.log_manager.append(lsn)
+        
+    
--- simple-db/transaction/recovery/set_string_record.py
+++ simple-db/transaction/recovery/set_string_record.py
@@ -5,5 +5,68 @@ from file.page import Page
 from file.block_id import BlockId
 
 class SetStringRecord(LogRecord):
-    def __init__(self):
-        super().__init__()
+    def __init__(self, page:Page):
+        """
+        Creates a new setstring log record.
+        :param page: the page containing the log record
+        """
+        tpos = 4
+        self.tx_num = page.get_int(tpos)
+        
+        fpos = tpos + 4
+        self.filename = page.get_string(fpos)
+        
+        bpos = fpos + Page.max_length(len(self.filename))
+        self.block_num = page.get_int(bpos)
+        self.block = BlockId(self.filename, self.block_num)
+        
+        opos = bpos + 4
+        self.offset = page.get_int(opos)
+        
+        vpos = opos + 4
+        self.val = page.get_string(vpos)
+        
+    
+    def op(self):
+        return self.SETSTRING
+    
+    def tx_number(self):
+        return self.tx_num
+    
+    def __str__(self):
+        return f'<SETSTRING {self.tx_num} {self.block} {self.offset} {self.val}>'
+    
+    def undo(self, transaction:Transaction):
+        """
+        Replace the specified data value with the value saved in this log record.
+        Pins a buffer to the specified block, calls set_string to restore the saved value
+        and unpins the buffer
+        """
+        transaction.pin(self.block)
+        transaction.set_string(self.block, self.offset, self.val, False)  # don't log the undo
+        transaction.unpin(self.block)
+        
+    def write_to_log(self, log_manager, tx_num, block:BlockId, offset, val):
+        """
+        Write a setstring record to the log.
+        This record contains the SETSTRING operator, followed by the transaction id, offset, value and old value.
+        :return: the LSN of the last log value
+        """
+        tpos = 4
+        fpos = tpos + 4
+        bpos = fpos + Page.max_length(len(self.filename))
+        opos = bpos + 4
+        vpos = opos + 4
+        
+        rec_len = vpos + Page.max_length(len(val))
+        rec = bytearray(rec_len)
+        
+        page = Page(rec)
+        page.set_int(0, self.SETSTRING)
+        page.set_int(tpos, tx_num)
+        page.set_string(fpos, self.filename)
+        page.set_int(bpos, block.number())
+        page.set_int(opos, offset)
+        page.set_string(vpos, val)
+        
+        return log_manager.append(rec)
