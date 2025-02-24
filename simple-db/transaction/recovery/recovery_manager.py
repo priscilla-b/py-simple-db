5ff771cceebe393f1fe0449161257bcd983b349c
Priscilla Baah <pyfbaah@gmail.com>
2025-02-24 22:32:33 +0000

    recovery manager in progress

--- simple-db/transaction/recovery/recovery_manager.py
+++ simple-db/transaction/recovery/recovery_manager.py
@@ -1,8 +1,12 @@
+from typing import Set, Iterator
+from collections import deque
+
 from start_record import StartRecord
 from buffer.buffer_manager import BufferManager
 from log.log_manager import LogManager
 from commit_record import CommitRecord
 from rollback_record import RollbackRecord
+from log_record import LogRecord
 from transaction.transaction import Transaction
 
 class RecoveryManager:
@@ -35,4 +39,38 @@ class RecoveryManager:
         lsn = RollbackRecord.write_to_log(self.log_manager, self.tx_number)
         self.log_manager.append(lsn)
         
+        
+    def recover(self):
+        """
+        Recover uncompleted transactions from the log
+        and then write a quiescent checkpoint record to the log and flush it.
+        """
+        self.do_recover()
+        self.buffer_manager.flush_all(self.tx_number)
+        lsn = RollbackRecord.write_to_log(self.log_manager, self.tx_number)
+        self.log_manager.flush(lsn)     
+    
+    def do_recover(self):
+        """
+        Do a complete database recovery.
+        Method iterates through the log records.
+        Whenever it finds a log record for an unfinished transaction,
+        it calls undo() on that record.
+        Method stops when it encounters a checkpoint record or the end of the log.
+        """
+        
+        finished_txs: Set[int] = set()
+        iter: Iterator = self.log_manager.iterator()
+        
+        for bytes_record in iter:
+            rec = LogRecord.create_log_record(bytes_record)
+            
+            if rec.op() == LogRecord.CHECKPOINT:
+                return  # stop recovery when we encounter a checkpoint record
+            
+            if rec.op() in {LogRecord.COMMIT, LogRecord.ROLLBACK}:
+                finished_txs.add(rec.tx_number())  # record that this transaction is finished
+            elif rec.tx_number() not in finished_txs:
+                rec.undo(self.transaction)  # undo the operation if the transaction wasn't committed/rolled back
+                
     
