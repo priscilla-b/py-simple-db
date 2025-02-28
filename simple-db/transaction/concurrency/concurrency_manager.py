06283318eb50132568c349e7a9454205d5150b95
Priscilla Baah <pyfbaah@gmail.com>
2025-02-28 23:34:43 +0000

    lock table implementation in progress

--- simple-db/transaction/concurrency/concurrency_manager.py
+++ simple-db/transaction/concurrency/concurrency_manager.py
@@ -3,3 +3,17 @@ from file.block_id import BlockId
 class ConcurrencyManager:
     def __init__(self):
         pass
+    
+    def x_lock(self, block):
+        """
+        Obtain an XLock on the block if necessary.
+        If the transaction does not have an XLock on that block,
+        then the method first gets an SLock on that block, and then
+        upgrades it to an XLock
+        
+        :param block: a reference to the disk block
+
+        """
+        if not self.has_s_lock(block):
+            self.s_lock(block)
+            self.lock_table.x
--- simple-db/transaction/concurrency/lock_table.py
+++ simple-db/transaction/concurrency/lock_table.py
@@ -22,6 +22,7 @@ class LockTable:
         Requests an SLock on the specified block.
         If the lock request cannot be granted, the method waits
         until it can be granted.
+        
         :param block: the reference to the disk block
         """
         
@@ -30,11 +31,37 @@ class LockTable:
             while self.has_x_lock(block) and not self.wating_too_long(timestamp):
                 time.sleep(self.MAX_TIME)
                 
-                if self.has_x_lock(block):
-                    raise RuntimeError() # will change this to a LockAbortException later
+            if self.has_x_lock(block):
+                raise RuntimeError() # will change this to a LockAbortException later
                 
-                val = self.get_lock_val(block)
-                self.locks[block] = val + 1
+            val = self.get_lock_val(block)
+            self.locks[block] = val + 1
+            
         except Exception as e:   # will change to the specific exception later
             raise RuntimeError()
+        
+    
+    def x_lock(self, block):
+        """
+        Grant an XLock on the specified block.
+        If a lock of any type exists when the method is called,
+        then the calling thread will be placed on a wait list
+        until the locks are released.
+        If the thread remains on the wait list for a certain
+        amount of time, then an exception is thrown.
+        
+        :param block: a reference to the disk block
+
+        """
+        try:
+            timestamp = int(time.time() * 1000) 
+            while self.has_other_s_locks(block) and not self.waiting_too_long(timestamp):
+                time.sleep(self.MAX_TIME)
+            
+            if self.has_other_s_locks(block):
+                raise RuntimeError()
+            self.locks[block] = -1
+        except Exception as e:
+            raise RuntimeError()
+        
     
