47916ee7c83c81ce789ca1614704d530e3887f1f
Priscilla Baah <pyfbaah@gmail.com>
2025-03-12 23:49:31 +0000

    update

--- simple-db/record/schema.py
+++ simple-db/record/schema.py
@@ -1,3 +1,4 @@
+import enum
 
 class Schema:
     
@@ -18,7 +19,48 @@ class Schema:
         self.info[field_name] = FieldInfo(field_type, length)
         
 
+    def add_int_field(self, field_name):
+        """Add an integer field to the schema
+        
+        :param fieldname: the name of the field
+
+        """
+        self.add_field(field_name, SqlType.INTEGER, 0)
+        
+    def add_string_field(self, field_name, length):
+        """
+        Add a string field to the schema
+        The length of the string is specified by the length parameter
+        e.g if a field is defined as varchar(10), then the length is 10
+        
+        :param fieldname: the name of the field
+        :param length: the length of the field
+
+        """
+        self.add_field(field_name, SqlType.VARCHAR, length)
+        
+    
+    def add(self, fieldname, schema):
+        """
+        Add a field to the schema with the same type and length as 
+        the corresponding field in another schema
+
+        Args:
+            fieldname (string): the name of the field
+            schema (Schema): the other schema
+        """
+        
+        _type = schema.type(fieldname)
+        length = schema.length(fieldname)
+        self.add_field(fieldname, _type, length)
+        
+        
+
 class FieldInfo:
     def __init__(self, field_type, length):
         self.field_type = field_type
         self.length = length
+        
+class SqlType(enum.IntEnum):
+    INTEGER = 4  
+    VARCHAR = 12  
